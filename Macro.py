349ab64818979bedb48ac263565b0c5fdfa64e48
sphnix <120174825+Sphnixken@users.noreply.github.com>
2024-01-24 16:20:05 +0530

    Added Spotify Macro for Sphnix

--- Macro.py
+++ Macro.py
@@ -1,11 +1,18 @@
 import pyautogui
 import time
-#Modules
+
+# Modules
 
 
 user = input("Hi, Pls tell who is using (Sphnix or Symprax) : ")
 if user == "Sphnix" or "sphnix":
-    print("SID")
-    
+    Sphnix_Choice = input("Code Bruv")
+    if Sphnix_Choice == "Spotify":
+        pyautogui.click(860, 1064)
+        time.sleep(4)
+        pyautogui.click(222, 449)
+        time.sleep(2)
+        pyautogui.click(482, 422)
+
 if user == "Symprax" or "symprax":
     print("SID")
