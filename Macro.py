8b2e4e0869e8ccef07aedf42191cfd3fb3bb5722
sphnix <120174825+Sphnixken@users.noreply.github.com>
2024-01-24 15:56:19 +0530

    Added Modules and Made the input function

--- Macro.py
+++ Macro.py
@@ -1,1 +1,11 @@
+import pyautogui
+import time
+#Modules
 
+
+user = input("Hi, Pls tell who is using (Sphnix or Symprax) : ")
+if user == "Sphnix" or "sphnix":
+    print("SID")
+    
+if user == "Symprax" or "symprax":
+    print("SID")
